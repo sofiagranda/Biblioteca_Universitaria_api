64603280a12b4a85413b518a15a64d643c8b719e
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-11-25 19:45:57 -0500

    Tercer Commit

--- Biblioteca_Universitaria_api/Biblioteca_Universitaria_api/urls.py
+++ Biblioteca_Universitaria_api/Biblioteca_Universitaria_api/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from libros.views import LibroViewSet
+from libros.views import LibroViewSet,prestamos_semana
 
 router = DefaultRouter()
 router.register(r'libros', LibroViewSet)
@@ -10,4 +10,5 @@ router.register(r'libros', LibroViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('prestamos/semana', prestamos_semana),
 ]
--- Biblioteca_Universitaria_api/libros/views.py
+++ Biblioteca_Universitaria_api/libros/views.py
@@ -8,3 +8,27 @@ from rest_framework.response import Response
 class LibroViewSet(viewsets.ModelViewSet):
     queryset = Libro.objects.all()
     serializer_class = LibroSerializer
+    
+@api_view(['POST'])
+def prestamos_semana(request):
+    prestamos = request.data.get('prestamosPorDia', [])
+    if len(prestamos) != 7:
+        return Response({"error": "Debe enviar 7 valores (uno por día)"}, status=400)
+
+    total = sum(prestamos)
+    promedio = total / 7
+
+    if total < 10:
+        mensaje = "Poca actividad de préstamo"
+    elif 10 <= total <= 30:
+        mensaje = "Actividad normal"
+    else:
+        mensaje = "Alta demanda de libros"
+
+    return Response({
+        "totalPrestamos": total,
+        "promedioDiario": promedio,
+        "mensaje": mensaje
+    })
+
+
